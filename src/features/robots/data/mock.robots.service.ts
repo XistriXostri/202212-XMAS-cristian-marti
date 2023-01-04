a357c590870a07e04c198118389ac45691ce14cb
xistri <cristian93marti@gmail.com>
2023-01-04 17:18:44 +0100

    add service storage and testing

---
+++ src/features/robots/data/mock.robots.service.ts
@@ -0,0 +1,34 @@
+import {
+    getStorageList,
+    setStorageList,
+} from '../../../core/services/storage/storage';
+import { consoleDebug } from '../../../tools/debug';
+import { Robot, RobotStructure } from '../models/robot';
+import { ROBOTS } from './mock.robots';
+
+export const getRobots = async (): Promise<Array<RobotStructure>> => {
+    const data = getStorageList<Robot>('Robots');
+    if (!data.length) {
+        setStorageList('Robots', ROBOTS);
+        return ROBOTS;
+    }
+    return data;
+};
+
+export const getRobotsDelay = (): Promise<Array<RobotStructure>> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const data = getStorageList<Robot>('Robots');
+            if (!data.length) {
+                setStorageList('Notes', ROBOTS);
+                resolve(ROBOTS);
+            }
+            resolve(data);
+        }, 2000);
+    });
+};
+
+export const saveRobots = async (Robots: Array<Robot>) => {
+    consoleDebug('Saving');
+    setStorageList('Robots', Robots);
+};
